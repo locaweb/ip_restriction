74a1d9bd7d17ea1c70664d7da80934fbf1646f53
Bruno Alvares da Costa <brunoadacosta@gmail.com>
2015-07-23 20:11:23 -0300

    add middleware to use in rack apps

---
+++ lib/ip_restriction/middleware.rb
@@ -0,0 +1,27 @@
+require 'singleton'
+
+module IpRestriction
+  class Middleware
+    def initialize(app)
+      @app = app
+    end
+
+    def call(env)
+      constraint = Constraint.instance
+
+      if constraint.checker.allowed?(env["REMOTE_ADDR"])
+        @app.call(env)
+      else
+        [404, {}, ['']]
+      end
+    end
+
+    class Constraint
+      include Singleton
+
+      def checker
+        @checker ||= IpRestriction::IpChecker.new(IpRestriction::IpsRetriever.new.office_ips)
+      end
+    end
+  end
+end
